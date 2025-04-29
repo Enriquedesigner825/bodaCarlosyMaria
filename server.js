d0af0dde72d725d9eb370d93697acbacdf6db521
Enrique <25wike25@gmail.com>
2025-04-29 16:31:50 +0200

    Definir ruta para la raíz

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 app.get('/', (req, res) => {
